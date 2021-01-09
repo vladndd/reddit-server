4de13926b21133de35b6dc10af68bb525836d6a9
asce-del <vlad.yaromiy@gmail.com>
2021-01-09 16:25:03 +0200

    graphql & mikroorm crud

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./consts";
-import {__port__} from "./consts";
-import { Post } from "./entities/Post";
+import { __port__ } from "./consts";
 import microConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
+
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
   app.listen(__port__, () => {
-    console.log(`Server started on port ${__port__}`)
-  })
+    console.log(`Server started on port ${__port__}`);
+  });
 };
 
 main().catch((error) => console.error(error));
